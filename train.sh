23a80702770d29607cca3c42e4aa27c0bd7ef47e
Takuma Mori <takuma104@gmail.com>
2026-04-04 21:45:45 +0900

    Run1

--- train.sh
+++ train.sh
@@ -2,20 +2,21 @@
 
 GPU_IDS="0"
 NUM_GPUS=1
+RUN_NUMBER=1
 
 # Training config file
-TRAIN_CONFIG="examples/config/train_config_emilia.json"
+TRAIN_CONFIG="output/run${RUN_NUMBER}/train.json"
 
 # Data config file
-data_config="examples/config/data_config_emilia_.json"
+data_config="output/run${RUN_NUMBER}/data.json"
 
 # Output directory for checkpoints
-OUTPUT_DIR="exp/omnivoice_emilia"
+OUTPUT_DIR="output/run${RUN_NUMBER}"
 
 uv run accelerate launch \
- --gpu_ids "${GPU_IDS}" \
-        --num_processes ${NUM_GPUS} \
-        -m omnivoice.cli.train \
-        --train_config ${TRAIN_CONFIG} \
-        --data_config ${data_config} \
-        --output_dir ${OUTPUT_DIR}
+       --gpu_ids "${GPU_IDS}" \
+       --num_processes ${NUM_GPUS} \
+       -m omnivoice.cli.train \
+       --train_config ${TRAIN_CONFIG} \
+       --data_config ${data_config} \
+       --output_dir ${OUTPUT_DIR}
